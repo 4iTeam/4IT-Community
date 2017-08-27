700482811db71ae00fe1b9a308c7ade63d5bf6e0
4iTeam <4iteam@vui360.net>
2017-08-27 21:58:00 +0700

    Rank reverse

--- rank.js
+++ rank.js
@@ -4,6 +4,7 @@
         el: "#ranking",
         data: {
             endpoint: "https://4it.top/api/v1/rank",
+            action:'top',
             ranks: [],
             uid: '',
             gid: '1415192401896193',
@@ -13,15 +14,18 @@
             loading: !1
         },
         mounted: function () {
+            var action;
+            if(action=$(this.$el).data('action')){
+                this.action=action;
+            }
             this.loadRank();
-
         },
         updated:function(){
             $('table').bootstrapTable();
         },
         methods: {
             loadRank: function () {
-                this.$http.get(this.endpoint + '/top' + '?gid=' + this.gid).then(function (r) {
+                this.$http.get(this.endpoint + '/' + this.action + '?gid=' + this.gid).then(function (r) {
                     return r.json();
                 }).then(function (r) {
                     if(r.success) {
